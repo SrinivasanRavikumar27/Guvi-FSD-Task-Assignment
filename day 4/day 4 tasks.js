953ce200cb7c8b0217d882b2b182dd93e04d3df2
SrinivasanRavikumar27 <tosrinivasanravi@gmail.com>
2023-08-31 20:10:33 +0530

    local to github repository

--- day 4/day 4 tasks.js
+++ day 4/day 4 tasks.js
@@ -165,26 +165,113 @@ solution();
 
 //  iife function
 
-
+(function(){
+   let arr1 = [1,4,7];
+   let arr2 = [2,6,8];
+   let a = arr1.length;
+   let b = Math.floor(a/2);
+   let c = (arr1[b] + arr2[b])/2;
+   console.log(c);
+})();
 
 //  anonymous function
 
+var solution = function(){
+   let arr1 = [1,4,7];
+   let arr2 = [2,6,8];
+   let a = arr1.length;
+   let b = Math.floor(a/2);
+   let c = (arr1[b] + arr2[b])/2;
+   console.log(c);
+}
+solution();
 
 //////  g) Remove duplicates from an array
 
 //  iife function
 
+(function(){
+   let arr = [1,2,3,3,4,5,5,3,6,7,7];
+  let res = Array.from(new Set(arr));
+  console.log(res);
+})();
+
 //  anonymous function
 
+var solution = function(){
+   let arr = [1,2,3,3,4,5,5,3,6,7,7];
+  let res = Array.from(new Set(arr));
+  console.log(res);
+}
+solution();
 
 //////////  h) Rotate an array by k times
 
 //  iife function
 
+(function(){
+   let arr = [1,2,3,4,5];
+   let k = 2;
+   let n = arr.length;
+   for(var i = 0;i < k;i++){
+       var lastElement = arr[n-1];
+       for(var j = n - 1; j > 0 ;j--){
+           arr[j] = arr[j-1];
+       }
+       arr[0] = lastElement;
+       var res = arr;
+   }
+  
+  console.log(res);
+})();
+
 //  anonymous function
 
+var solution = function(){
+   let arr = [1,2,3,4,5];
+   let k = 2;
+   let n = arr.length;
+   for(var i = 0;i < k;i++){
+       var lastElement = arr[n-1];
+       for(var j = n - 1; j > 0 ;j--){
+           arr[j] = arr[j-1];
+       }
+       arr[0] = lastElement;
+       var res = arr;
+   }
+  
+  console.log(res);
+}
+solution();
 
 
+// Programs in Arrow function
+
+
+//  a) Print odd numbers in an array
+
+var b = Array.from({length:11},(_,i) => i).filter(i => i%2 !== 0).join(' ');
+console.log(b)
+
+//  b) Convert all the strings to title caps in a string array
+
+var b = "the name is srini".split(' ').map(word => word[0].toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+console.log(b)
+
+//  c) Sum of all numbers in an array
+
+var b = [1,2,3,4,5].reduce((acc,curr) => acc + curr,0);
+console.log(b)
+
+//  d) Return all the prime numbers in an array
+
+var b = Array.from({length:20},(_,i) => i+1).filter(num => num >1 && (num === 2 || Array.from({length:Math.floor(Math.sqrt(num))},(_,j) => j+2).every(d => num%d !== 0)));
+console.log(b)
+
+//  e) Return all the palindromes in an array
+
+var b = str => str === str.split('').reverse().join('');
+console.log(b('madam') ? 'it is palindrome' : 'it is palindrome');
 
 
 
